4d1f371ddaf9859a5d14cf16d0e9cd93bed52fb6
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-09-16 16:46:30 -0700

    Organizing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
+  resources :nara_casefile
+  resources :search_casefiles
+
+  root 'application#index'
   get 'searchcase/search'
-
   get 'searchcase/display'
-
 	get 'searchcase/fullDisplay'
-
   get 'searchcase/recordNotFound'
-
-  #Seems removable
   get 'nara_casefile/index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  resources :nara_casefile
-	resources :search_casefiles
-
-   root 'application#hello'
-
+   get '*path' => redirect('/')
 end
