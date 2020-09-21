766267430ac87d27818aa3392ffc82915a077cfb
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-09-21 11:52:04 -0700

    Tests and quality of life updates to view

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe 'Navigate NARA search', type: :feature do
     visit '/searchcase/search'
     expect(page).to have_content('The Early Arrivals Records Search (EARS)')
   end
+
+  scenario 'Root re-directs to search' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+  end
+
+  scenario 'empty search shows all results' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    click_button "Search"
+    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+  end
+
+  scenario 'Display Shows intended view' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    click_button "Search"
+    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+   first(:link, "Display").click
+  end
 end
