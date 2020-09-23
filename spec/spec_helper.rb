cfed304235639a2f0ffd7195c7a62595c61bdf13
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-09-23 09:37:15 -0700

    removing env var reset

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,5 @@ ENV['RAILS_ENV'] = 'test' unless ENV['RAILS_ENV']
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 require 'capybara/rspec'
-system "rails RAILS_ENV=test  db:drop db:create db:migrate"
+system "rails db:drop db:create db:migrate"
 
