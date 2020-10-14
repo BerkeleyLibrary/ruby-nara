c477ef64c0a5c639156ce286b4fb9ed972dece6b
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-10-14 14:54:59 -0700

    sass setup

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+# frozen_string_literal:true
+
+Rails.application.config.assets.version = '1.0'
+
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+Rails.application.config.assets.precompile += %w[*.png]
+
+Rails.application.config.assets.precompile += %w( application.css )
