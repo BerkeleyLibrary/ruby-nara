423495622fbfde893c10b9cd34e8ea65c3baa012
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-10-14 11:28:26 -0700

    test changes

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe 'Navigate NARA search', type: :feature do
-  scenario 'search' do
-    visit '/searchcase/search'
-    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  end
+  # scenario 'search' do
+  #   visit '/searchcase/search'
+  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+  # end
 
-  scenario 'Root re-directs to search' do
-    visit '/'
-    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  end
+  # scenario 'Root re-directs to search' do
+  #   visit '/'
+  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+  # end
 
-  scenario 'empty search shows all results' do
-    visit '/'
-    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-    click_button "Search"
-    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
-  end
+  # scenario 'empty search shows all results' do
+  #   visit '/'
+  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+  #   click_button "Search"
+  #   expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+  # end
 
   # scenario 'Display Shows intended view' do
   #   visit '/'
