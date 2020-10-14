4e3cd27001fbd27a1275e2d3b2834a17f5d0982e
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-10-14 11:35:06 -0700

    test changes

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 RSpec.describe 'Navigate NARA search', type: :feature do
-  # scenario 'search' do
-  #   visit '/searchcase/search'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  # end
+  scenario 'search' do
+    visit '/searchcase/search'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+  end
 
   # scenario 'Root re-directs to search' do
   #   visit '/'
