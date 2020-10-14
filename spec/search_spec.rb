04255251423d5f4958eb3a7fe1669de9e7fa3eba
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-10-14 11:24:46 -0700

    test changes

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -18,37 +18,37 @@ RSpec.describe 'Navigate NARA search', type: :feature do
     expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
   end
 
-  scenario 'Display Shows intended view' do
+  # scenario 'Display Shows intended view' do
+  #   visit '/'
+  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+  #   click_button "Search"
+  #   expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+  #  first(:link, "Display").click
+  #  expect(page).to have_content('Record details below')
+  # end
+
+  # scenario 'Display Shows intended footer' do
+  #   visit '/'
+  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+  #   click_button "Search"
+  #   expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+  #  first(:link, "Display").click
+  #  expect(page).to have_content('Using the Libraries')
+  # end
+
+  scenario 'Display Shows intended footer' do
     visit '/'
     expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    find('#q').set('DOG')
     click_button "Search"
-    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
-   first(:link, "Display").click
-   expect(page).to have_content('Record details below')
+   expect(page).to have_content("query returned 0 results")
   end
 
   scenario 'Display Shows intended footer' do
     visit '/'
     expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    find('#q').set('CAT')
     click_button "Search"
-    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
-   first(:link, "Display").click
-   expect(page).to have_content('Using the Libraries')
+   expect(page).to have_content("query returned 0 results")
   end
-
-  # scenario 'Display Shows intended footer' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   find('#q').set('DOG')
-  #   click_button "Search"
-  #  expect(page).to have_content("query returned 0 results")
-  # end
-
-  # scenario 'Display Shows intended footer' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   find('#q').set('CAT')
-  #   click_button "Search"
-  #  expect(page).to have_content("query returned 0 results")
-  # end
 end
