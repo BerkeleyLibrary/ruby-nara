cc8b3d7ed5d4e23893300bcc1142412a681fa0a5
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-10-16 09:23:50 -0700

    Turning tests back on

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -6,49 +6,42 @@ RSpec.describe 'Navigate NARA search', type: :feature do
     expect(page).to have_content('The Early Arrivals Records Search (EARS)')
   end
 
-  # scenario 'Root re-directs to search' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  # end
+  scenario 'Root re-directs to search' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+  end
 
-  # scenario 'empty search shows all results' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   click_button "Search"
-  #   expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
-  # end
+  scenario 'empty search shows all results' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    click_button "Search"
+    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+  end
 
-  # scenario 'Display Shows intended view' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   click_button "Search"
-  #   expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
-  #  first(:link, "Display").click
-  #  expect(page).to have_content('Record details below')
-  # end
+  scenario 'Display Shows intended view' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    click_button "Search"
+    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+   first(:link, "Display").click
+   expect(page).to have_content('Record details below')
+  end
 
-  # scenario 'Display Shows intended footer' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   click_button "Search"
-  #   expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
-  #  first(:link, "Display").click
-  #  expect(page).to have_content('Using the Libraries')
-  # end
+  scenario 'Display Shows intended footer' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    click_button "Search"
+    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+   first(:link, "Display").click
+   expect(page).to have_content('Using the Libraries')
+  end
 
-  # scenario 'Display Shows intended footer' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   find('#q').set('DOG')
-  #   click_button "Search"
-  #  expect(page).to have_content("query returned 0 results")
-  # end
+  scenario 'Display Shows intended footer' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    find('#q').set('DOG')
+    click_button "Search"
+   expect(page).to have_content("query returned 0 results")
+  end
 
-  # scenario 'Display Shows intended footer' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   find('#q').set('CAT')
-  #   click_button "Search"
-  #  expect(page).to have_content("query returned 0 results")
-  # end
 end
