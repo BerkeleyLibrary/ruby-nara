b18757fb0b77e8eb7be5473a0d8c097e94a84999
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-10-19 11:45:15 -0700

    removing breaking call to seed data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 require 'capybara/rspec'
 
-load "#{Rails.root}/db/seeds.rb"
+# load "#{Rails.root}/db/seeds.rb"
 
 def set_up
     Rails.application.load_seed
