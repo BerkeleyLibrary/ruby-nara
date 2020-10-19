bbfb890914351cdb51005a6a86923ea1e96fed6b
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-10-19 12:38:04 -0700

    moving record setup to spec file

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe 'Navigate NARA search', type: :feature do
+
+  before(:all)  do
+    NARA_CaseFiles = NaraCasefile.create(CaseID: 111, ID: 2,BOXNUMBER: "Test",LASTNAME: "TEST", FIRSTNAME: "TEST");
+  end
+
   scenario 'search' do
     visit '/searchcase/search'
     expect(page).to have_content('The Early Arrivals Records Search (EARS)')
@@ -11,37 +16,37 @@ RSpec.describe 'Navigate NARA search', type: :feature do
     expect(page).to have_content('The Early Arrivals Records Search (EARS)')
   end
 
-  # scenario 'empty search shows all results' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   click_button "Search"
-  #   expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
-  # end
+  scenario 'empty search shows all results' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    click_button "Search"
+    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+  end
 
-  # scenario 'Display Shows intended view' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   click_button "Search"
-  #   expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
-  #  first(:link, "Display").click
-  #  expect(page).to have_content('Record details below')
-  # end
+  scenario 'Display Shows intended view' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    click_button "Search"
+    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+   first(:link, "Display").click
+   expect(page).to have_content('Record details below')
+  end
 
-  # scenario 'Display Shows intended footer' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   click_button "Search"
-  #   expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
-  #  first(:link, "Display").click
-  #  expect(page).to have_content('© University of California Regents. All rights reserved')
-  # end
+  scenario 'Display Shows intended footer' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    click_button "Search"
+    expect(page).to have_content('LastName FirstName MiddleName Age Gender Ship Port Date Destination')
+   first(:link, "Display").click
+   expect(page).to have_content('© University of California Regents. All rights reserved')
+  end
 
-  # scenario 'Display Shows intended footer' do
-  #   visit '/'
-  #   expect(page).to have_content('The Early Arrivals Records Search (EARS)')
-  #   find('#q').set('DOG')
-  #   click_button "Search"
-  #  expect(page).to have_content("query returned 0 results")
-  # end
+  scenario 'Display Shows intended footer' do
+    visit '/'
+    expect(page).to have_content('The Early Arrivals Records Search (EARS)')
+    find('#q').set('DOG')
+    click_button "Search"
+   expect(page).to have_content("query returned 0 results")
+  end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,3 @@ ENV['RAILS_ENV'] = 'test' unless ENV['RAILS_ENV']
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 require 'capybara/rspec'
-
-load "#{Rails.root}/db/seeds.rb"
-
-def set_up
-    Rails.application.load_seed
-end
