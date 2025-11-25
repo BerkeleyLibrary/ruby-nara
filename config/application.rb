ca9adb187b8009aac9b30f97cf904b96ecb22f28
María A. Matienzo <matienzo@berkeley.edu>
2025-11-24 19:01:46 -0800

    rails 7.2

--- config/application.rb
+++ config/application.rb
@@ -12,24 +12,19 @@ Bundler.require(*Rails.groups)
 module NewNara
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
-    # ############################################################
-    # Customize logging
-
-    # require 'berkeley_library/logging'
-    # LibIT::Logging.configure!
-     # Please, add to the `ignore` list any other `lib` subdirectories that do
-     # not contain `.rb` files, or that should not be reloaded or eager loaded.
-     # Common ones are `templates`, `generators`, or `middleware`, for example.
-     config.autoload_lib(ignore: %w(assets tasks))
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
  
-     # Configuration for the application, engines, and railties goes here.
-     #
-     # These settings can be overridden in specific environments using the iles
-     # in config/environments, which are processed later.
-     #
-     # config.time_zone = "Central Time (US & Canada)"
-     # config.eager_load_paths << Rails.root.join("extras")
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the iles
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
